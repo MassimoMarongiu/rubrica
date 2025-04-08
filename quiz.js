5dbc5c342883dcddade5a117343398161910e74f
MassimoMarongiu <46823233+MassimoMarongiu@users.noreply.github.com>
2025-04-08 19:54:30 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,31 +1,34 @@
 "use strict";
 
-class domande {
-    constructor(domanda, risposta) {
-        this.domanda = domanda;
-        this.risposta = risposta;
+let domande = [
+    {
+        domanda: "Qual è il mio colore preferito?@\nRisposta: 1)Blu 2)Rosso 3)Verde",
+        risposta: "1"
+    },
+    {
+        domanda: "Qual è il tuo animale preferito?@\nRisposta: 1)Gatto 2)Cane 3)Gallina",
+        risposta: "1"
     }
-}
-let quiz = [];
+];
+
 let risposteUtente = [];
-let domande1 = new domande("Qual è il mio colore preferito?\nRisposta: 1)Blu 2)Rosso 3)Verde", "1");
-let domande2 = new domande("Qual è il tuo animale preferito?\nRisposta: 1)Gatto 2)Cane 3)Gallina", "1");
-quiz.push(domande1, domande2);
-console.log(quiz);
-for (let i = 0; i < quiz.length; i++) {
-        risposteUtente[i] = prompt(quiz[i].domanda +"\n inserire un numero tra 1 e 3")
-        parseInt(risposteUtente[i]);
-  if(isNaN(risposteUtente[i]) || risposteUtente[i].length < 1 || risposteUtente[i].length > 1
-    || risposteUtente[i]  < 1 || risposteUtente[i]  > 3) {
-        alert("devi inserire un numero tra 1 e 3");
-        i--;
-    }
-}
 let score = 0;
-risposteUtente.forEach((rispostaUtente, index) => {
-    alert(quiz[index].domanda + "\n hai risposto: "+risposteUtente[rispostaUtente]);
-    if (risposteUtente[rispostaUtente] === quiz[index].risposta) {
+
+for (let i = 0; i < domande.length; i++) {
+    let rispostaUtente = prompt("Domanda" + (i + 1) + " " + domande[i].domanda + "\n inserire un numero tra 1 e 3");
+    risposteUtente.push(rispostaUtente);
+    if (rispostaUtente === domande[i].risposta) {
         score++;
     }
-});
-alert("Hai totalizzato "+score+"su "+quiz.length+" domande");
+}
+
+for (let i = 0; i < domande.length; i++) {
+    let index = domande[i].domanda.indexOf("@");
+    if (risposteUtente[i] === domande[i].risposta) {
+        alert("Domanda" + (i + 1) + " " + domande[i].domanda.substring(0, index) + "\n hai risposto: " + risposteUtente[i] + "\n Risposta corretta ");
+    } else {
+        alert("Domanda " + (i + 1) + " " + domande[i].domanda.substring(0, index) + "\n hai risposto: " + risposteUtente[i] + "\n Risposta errata ");
+    }
+}
+
+alert("Hai totalizzato " + score + "su " + domande.length + " domande");
